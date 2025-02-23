e62ab58c57741544722201a212269071cf8104ad
denzil-daniels-204525918 <denzildaniels@gmail.com>
2025-02-23 04:05:46 +0200

    files added via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to append value to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to evaluate the expression
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
